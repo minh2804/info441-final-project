2fc6659c47b24100f7e9c2cb3e9e3405cdf1f108
xuwensi <xuwensi0515@gmail.com>
2021-06-01 17:18:42 +0800

    wensi

--- server/todoList/todoList.go
+++ server/todoList/todoList.go
@@ -23,7 +23,6 @@ type MysqlStore struct {
 	DB *sql.DB
 }
 
-
 func (sql *MysqlStore) TaskPostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -86,9 +85,9 @@ func (sql *MysqlStore) TaskPatchHandler(w http.ResponseWriter, r *http.Request) 
 			}
 		}
 		w.Write([]byte("update is successful"))
-  }
+	}
 }
-    
+
 func (sql *MysqlStore) GetTaskHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
