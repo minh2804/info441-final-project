6e29b70e7bc25ef43fc1661871da5c96dd9a9b81
Han Chen <chenhan@uw.edu>
2021-06-01 16:46:42 +0800

    han branch

--- server/todoList/todoList.go
+++ server/todoList/todoList.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
+	"path"
 	"time"
 )
 
@@ -15,4 +18,69 @@ type TaskItem struct {
 
 type MysqlStore struct {
 	DB *sql.DB
+}
+
+func (sql *MysqlStore) TaskPostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	} else {
+		task := &TaskItem{}
+		err := json.NewDecoder(r.Body).Decode(&task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		insq := "INSERT INTO Tasks (UserId, TaskName, TaskType, InitTime) VALUES (?, ?, ?, ?)"
+		res, err := sql.DB.Exec(insq, task.UserID, task.TaskName,
+			task.TaskType, time.Now())
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		} else {
+			id, err := res.LastInsertId()
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				http.Error(w, err.Error(), http.StatusForbidden)
+				return
+			} else {
+				task.ID = id
+			}
+		}
+		w.Write([]byte("post is successful"))
+	}
+}
+
+func (sql *MysqlStore) TaskPatchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PATCH" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	} else {
+		taskId := path.Base(r.URL.Path)
+		task := &TaskItem{}
+		err := json.NewDecoder(r.Body).Decode(&task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		insq := "Update Tasks SET UserId=?, TaskName=?, TaskType=? WHERE TaskId=?"
+		res, err := sql.DB.Exec(insq, task.UserID, task.TaskName,
+			task.TaskType, taskId)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		} else {
+			id, err := res.LastInsertId()
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				http.Error(w, err.Error(), http.StatusForbidden)
+				return
+			} else {
+				task.ID = id
+			}
+		}
+		w.Write([]byte("update is successful"))
+	}
 }
