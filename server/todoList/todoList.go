c8ba1ef5ef476287ee4091be4a5b9ee0be7906fc
xuwensi <xuwensi0515@gmail.com>
2021-06-01 15:29:39 +0800

    wensi

--- server/todoList/todoList.go
+++ server/todoList/todoList.go
@@ -1,1 +1,25 @@
 package server
+
+import (
+	"database/sql"
+	"time"
+)
+
+// Task
+// TaskId int not null auto_increment primary key,
+// UserId int not null,
+// TaskName varchar(255) not null UNIQUE,
+// TaskType varchar(255) not null,
+// InitTime time not null
+
+type TaskItem struct {
+	ID       int64     `json:"id"`
+	UserID   int64     `json:"userId"`
+	TaskName string    `json:"taskName"`
+	TaskType string    `json:"taskType"`
+	InitTime time.Time `json:"initTime"`
+}
+
+type MysqlStore struct {
+	DB *sql.DB
+}
