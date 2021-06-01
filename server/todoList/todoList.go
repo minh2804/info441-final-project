1bfb542eb57223699b6a7517a4525a275f19c017
Han Chen <chenhan@uw.edu>
2021-06-01 15:31:58 +0800

    todo

--- server/todoList/todoList.go
+++ server/todoList/todoList.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"database/sql"
+	"time"
+)
+
+type TaskItem struct {
+	ID       int64     `json:"id"`
+	UserID   int64     `json:"userId"`
+	TaskName string    `json:"taskName"`
+	TaskType string    `json:"taskType"`
+	InitTime time.Time `json:"initTime"`
+}
+
+type MysqlStore struct {
+	DB *sql.DB
+}
