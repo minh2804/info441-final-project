48683c6a7801e7a02c5f5bd0f16ce4551c39b2dc
Kevin Chou <kchou0907@gmail.com>
2021-06-02 20:35:25 -0700

    patially finished store.go

--- servers/todolist/models/stats/store.go
+++ servers/todolist/models/stats/store.go
@@ -8,14 +8,11 @@ var ErrTaskNotFound = errors.New("task not found")
 
 // Store represents a store for Stats
 type Store interface {
-	// Get all the tasks the user has completed total
+	// Get all the tasks the user has created total
 	GetAllByID(userID int64) ([]*Task, error)
 
 	// Get all the tasks the user added this year
 	GetAllWithinYear(userID int64) ([]*Task, error)
-
-	// Get all the tasks the user added this month
-	GetAllWithinMonth(userID int64) ([]*Task, error)
 
 	// Get all the tasks the user added this week
 	GetAllWithinWeek(userID int64) ([]*Task, error)
@@ -28,9 +25,6 @@ type Store interface {
 
 	// Get all the tasks the user completed this year
 	GetCompletedWithinYear(userID int64) ([]*Task, error)
-
-	// Get all the tasks the user completed this month
-	GetCompletedWithinMonth(userID int64) ([]*Task, error)
 
 	// Get all the tasks the user completed this week
 	GetCompletedWithinWeek(userID int64) ([]*Task, error)
